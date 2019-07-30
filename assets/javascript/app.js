616139c12edb06a007732acc809f711c529bccbf
Helen Kim <Helenk490@gmail.com>
2019-07-29 20:52:50 -0400

    took loop out and used functions, option A done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,56 +6,10 @@ $(document).ready(function () {
     let answerThree = $("p.answerC")
     let begin = $("p.start")
     let timer = $("p.timer")
-    let number = 11;
+    let number = 6;
+
+
     let intervalId;
-
-    let roundOne = [
-        {
-            question: "Question?",
-            choiceOne: ["One", "Two", "Three"]
-        },
-        {
-            questionTwo: "Question 2?",
-            choiceTwo: ["one", "two", "three"]
-        },
-        {
-            questionThree: "Question 3?",
-            choiceThree: ["one", "two", "three"]
-        }
-    ]
-
-    let roundTwo = [
-        {
-            question: "Question?",
-            choice: ["One", "Two", "Three"]
-        },
-        {
-            questionTwo: "Question 2?",
-            choice: ["one", "two", "three"]
-        },
-        {
-            questionThree: "Question 3?",
-            choice: ["one", "two", "three"]
-        }
-    ]
-
-    let roundThree = [
-        {
-            question: "Question?",
-            choice: ["1", "2", "3"]
-        },
-        {
-            questionTwo: "Question 2?",
-            choice: ["one", "two", "three"]
-        },
-        {
-            questionThree: "Question 3?",
-            choice: ["one", "two", "three"]
-        }
-    ]
-
-
-
 
     function start() {
         begin.append("<button>Start</button>")
@@ -64,18 +18,212 @@ $(document).ready(function () {
 
     begin.on("click", function () {
         begin.hide();
-        for (let i=0; i<roundOne.length;i++) {
-        answer.append(roundOne[i].question)
-        answerTwo.append("<p>" + roundOne[i].choiceOne[0] + "</p>")
-        answerTwo.append("<p>" + roundOne[i].choiceOne[1] + "</p>")
-        answerTwo.append("<p>" + roundOne[i].choiceOne[2] + "</p>")
+
+        function roundTwoQuestionA() {
+            question.append("Phase Two Questions");
+            answer.append("<p>1. Phase Two Answer</p>");
+            answerTwo.append("<p>2. Phase Two Answer1</p>");
+            answerThree.append("<p>3. Phase Two Answer3</p>");
         }
+
+        function roundTwoQuestionC() {
+            question.append("Phase Three Questions");
+            answer.append("<p>1. Phase Three Answer</p>");
+            answerTwo.append("<p>2. Phase Three Answer1</p>");
+            answerThree.append("<p>3. Phase Three Answer3</p>");
+        }
+
+
+        function correctAnswerC() {
+            roundOneD();
+            question.append("<p>Correct Answer!</p>")
+            question.append("<img src= https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwRCWp3AhLRFZSCbz-qxDikRD4qST0DUFvxQcNqxRqMek8JHQZZQ>")
+        }
+
+        function correctAnswerB() {
+            roundOneC();
+            question.append("<p>Correct Answer!</p>")
+            question.append("<img src= https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwRCWp3AhLRFZSCbz-qxDikRD4qST0DUFvxQcNqxRqMek8JHQZZQ>")
+        }
+
+
+        function correctAnswerA() {
+            roundOneB();
+            question.append("<p>Correct Answer!</p>")
+            question.append("<img src= https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwRCWp3AhLRFZSCbz-qxDikRD4qST0DUFvxQcNqxRqMek8JHQZZQ>")
+
+        }
+
+        function wrongAnswer() {
+            question.append("<p>Wrong Answer!</p>")
+            question.append("<img src= https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwRCWp3AhLRFZSCbz-qxDikRD4qST0DUFvxQcNqxRqMek8JHQZZQ>")
+        }
+        function stop() {
+            clearInterval(intervalId);
+        }
+
+
+        function roundOneA() {
+            clearInterval(intervalId);
+            intervalId = setInterval(decrement, 1000);
+        }
+        function decrement() {
+            number--;
+            timer.text(number);
+            if (number === 0) {
+                stop();
+                $("p").empty();
+                wrongAnswer();
+            }
+        }
+
+
+
+        let numberOne = 6
+        function roundOneB() {
+            clearInterval(intervalId);
+            intervalId = setInterval(decrementB, 1000);
+        }
+        function decrementB() {
+            numberOne--;
+            timer.text(numberOne);
+            if (numberOne === 0) {
+                stop()
+                $("p").empty();
+                roundTwoQuestionA();
+                timerForQA();
+            }
+        }
+
+        let numberThree = 6;
+        function timerForQA() {
+            clearInterval(intervalId);
+            intervalId = setInterval(decrement, 1000);
+        }
+        function decrement() {
+            numberThree--;
+            timer.text(numberThree);
+            if (numberThree === 0) {
+                stop();
+                $("p").empty();
+                wrongAnswer();
+            }
+        }
+
+        let numberTwo = 6
+        function roundOneC() {
+            clearInterval(intervalId);
+            intervalId = setInterval(decrementC, 1000);
+        }
+        function decrementC() {
+            numberTwo--;
+            timer.text(numberTwo);
+            if (numberTwo === 0) {
+                stop()
+                $("p").empty();
+                roundTwoQuestionC();
+                timerForQC();
+            }
+        }
+
+
+        let numberFive = 6
+        function timerForQC() {
+            clearInterval(intervalId);
+            intervalId = setInterval(decrementE, 1000);
+        }
+        function decrementE() {
+            numberFive--;
+            timer.text(numberFive);
+            if (numberFive === 0) {
+                stop()
+                $("p").empty();
+                wrongAnswer();
+            }
+        }
+
+
+        function result() {
+            answer.append("Correct:")
+            answerTwo.append("Incorrect:")
+            answerThree.append("GAME OVER")
+        }
+
+
+        let numberFour = 6
+        function roundOneD() {
+            clearInterval(intervalId);
+            intervalId = setInterval(decrementD, 1000);
+        }
+        function decrementD() {
+            numberFour--;
+            timer.text(numberFour);
+            if (numberFour === 0) {
+                stop()
+                $("p").empty();
+                result();
+            }
+        }
+
+
+
+        roundOneA();
+        question.text("Question")
+        answer.text("One")
+        answerTwo.text("Two")
+        answerThree.text("Three")
+        answer.click(function () {
+            stop();
+            $("p").empty();
+            correctAnswerA();
+            answer.off('click').click(function () {
+                stop();
+                $("p").empty();
+                correctAnswerB();
+                answer.off('click').click(function () {
+                    stop();
+                    $("p").empty();
+                    correctAnswerC();
+                });
+            });
+
+
+
+
+
+
+            answerTwo.click(function () {
+                alert("clicked 2")
+                stop();
+
+            });
+            answerThree.click(function () {
+                alert("clicked 3")
+                stop();
+            });
+
+
+
+        });
+
+
+
     });
+    //     let userPick = $(this).val();
+    //     console.log(userPick)
+    //     if (userPick === answer) {
+    //         alert("if statement!")
+
+    //         // stop();
+    //         // $("p").empty();
+    //         // correctAnswer();
+    //     }
+    //     // alert("clicked 2")
+    //     // stop();
+    //     // $("p").empty();
+    // });
 
 
-    
-
-});
     // begin.on("click", function () {
     //     begin.hide();
     //     roundOneA();
@@ -180,4 +328,4 @@ $(document).ready(function () {
     //         wrongAnswer();
     //     }
     //}
-
+});
